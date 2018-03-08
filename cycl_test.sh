986a1c3695786ccbae577637abb7cade82a88d47
Lu Ken <bluewish.ken.lu@gmail.com>
2018-03-08 22:38:46 +0800

    refine

--- cycl_test.sh
+++ cycl_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tmpfile=$(mktemp /tmp/cycl_test.XXXXXX)
-cyclictest -l 1000 -m -S -h 400 > ${tmpfile}
+cyclictest -l 1000 -m -S -p99 -h 400 > ${tmpfile}
 python cycl_plot.py -f ${tmpfile}
 rm ${tmpfile}
